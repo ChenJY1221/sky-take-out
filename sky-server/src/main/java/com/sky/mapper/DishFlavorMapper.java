b8560e69e3a103212eba612488ec77dddf6798ac
CJY <2362090205@qq.com>
2024-11-09 21:28:25 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,12 +1,23 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface DishFlavorMapper {
-
+    /**
+     * 插入菜品口味
+     * @param flavors
+     */
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 根据菜品id删除相关的口味信息
+     * @param id
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
 }
