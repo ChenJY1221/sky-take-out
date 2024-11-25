a62cc83a56b2aeda12ef6436159df8a87fa94d34
CJY <2362090205@qq.com>
2024-11-25 22:55:48 +0800

    缓存套餐业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -26,12 +28,15 @@ public class DishController {
 
     @Autowired
     DishService dishService;
-
+    @Autowired
+    RedisTemplate redisTemplate;
     @PostMapping
     @ApiOperation("新增菜品")
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        String key = "dish_" + dishDTO.getCategoryId();
+        redisTemplate.delete(key);
         return Result.success();
     }
 
@@ -54,10 +59,12 @@ public class DishController {
      * @return
      */
     @DeleteMapping
-    @ApiOperation("删除菜品")
+    @ApiOperation("批量删除菜品")
     public Result delete(@RequestParam List<Long> ids) {
-        log.info("删除菜品：{}", ids);
+        log.info("批量删除菜品：{}", ids);
         dishService.deleteBatch(ids);
+        Set keys = redisTemplate.keys("dish_*");
+        redisTemplate.delete(keys);
         return Result.success();
     }
 
@@ -80,7 +87,10 @@ public class DishController {
         log.info("修改菜品：{}", dishDTO);
 
         dishService.updateWithFlavor(dishDTO);
-
+        Set keys = redisTemplate.keys("dish_*");
+        redisTemplate.delete(keys);
         return Result.success();
     }
+
+
 }
